595f334e200c748f5681beb1f20ade2c16db851d
hassa <hassa@DESKTOP-97N4AVF>
2022-05-18 15:11:14 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.edit_frist = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edit_last = new System.Windows.Forms.TextBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_edit = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(800, 362);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_delete);
+            this.panel1.Controls.Add(this.btn_edit);
+            this.panel1.Controls.Add(this.btn_add);
+            this.panel1.Controls.Add(this.edit_last);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.edit_frist);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 368);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 83);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Frist Name";
+            // 
+            // edit_frist
+            // 
+            this.edit_frist.Location = new System.Drawing.Point(109, 32);
+            this.edit_frist.Name = "edit_frist";
+            this.edit_frist.Size = new System.Drawing.Size(142, 20);
+            this.edit_frist.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(302, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Last  Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // edit_last
+            // 
+            this.edit_last.Location = new System.Drawing.Point(399, 31);
+            this.edit_last.Name = "edit_last";
+            this.edit_last.Size = new System.Drawing.Size(142, 20);
+            this.edit_last.TabIndex = 1;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(560, 47);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 2;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.Location = new System.Drawing.Point(641, 47);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(75, 23);
+            this.btn_edit.TabIndex = 2;
+            this.btn_edit.Text = "Edit";
+            this.btn_edit.UseVisualStyleBackColor = true;
+            this.btn_edit.Click += new System.EventHandler(this.btn_edit_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(722, 47);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_delete.TabIndex = 2;
+            this.btn_delete.Text = "Remove";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox edit_last;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox edit_frist;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_edit;
+        private System.Windows.Forms.Button btn_add;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    // notice: the name of the classes and the data are not recommended,
+    // but I focused on the concept rather than the performance.
+    public partial class Form1 : Form
+    {
+        // to define the table and the database entity
+        TB1 add;
+        Database1Entities db;
+        int id;
+        public Form1()
+        {
+            InitializeComponent();
+            loadData();
+        }
+
+        private void loadData()
+        {
+            try
+            {
+                // i made this code to Initialize it
+                db = new Database1Entities();
+                // gets or sets the source from the table the display it in the GridView
+                dataGridView1.DataSource = db.TB1.ToList();
+            }
+            catch
+            {
+                
+            }
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+
+       }
+
+      
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            /// <summary>
+            /// I made this function to add the Value
+            /// that you are including it to the gridview
+            /// </summary>
+            /// <param name="sender"></param>
+            /// <param name="e"></param>
+            try
+            {
+                // to Initialize the database and the table
+                db = new Database1Entities();
+                add = new TB1();
+                // the button add is = to the inputs frist name and last name 
+                add.Frist_Name = edit_frist.Text;
+                add.Last_Name = edit_last.Text;
+                // The entry provides acces to change tracking information which i made it add
+                db.Entry(add).State = System.Data.Entity.EntityState.Added;
+                db.SaveChanges();
+                // to define the data in the GridView
+                loadData();
+                //this is for make the input empty after pressing add button
+                edit_frist.Text = edit_last.Text = "";
+                //message after the app runs sucessfuly
+                MessageBox.Show("Added sucessfuly");
+            }
+            catch
+            {
+                MessageBox.Show("Can't Add your Value");
+            }
+        }
+        
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            /// <summary>
+            /// i made this function to edit the value in the Grid ,
+            /// once you double click in the row it returns the Values automatically
+            /// </summary>
+            /// <param name="sender"></param>
+            /// <param name="e"></param>
+            try
+            {   // 	Converts the string representation of a number in a specified base.
+                id = Convert.ToInt16(dataGridView1.CurrentRow.Cells[0].Value);
+                // to returns the Value from the GridView  
+                edit_frist.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[1].Value);
+                edit_last.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[2].Value);
+            }
+            catch
+            {
+                MessageBox.Show("Can't edit your Value");
+            }
+        }
+
+        private void btn_edit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                db = new Database1Entities();
+                add = new TB1();
+                add.Id = id;
+                add.Frist_Name = edit_frist.Text;
+                add.Last_Name = edit_last.Text;
+                // The entry provides acces to change tracking information which i made it modified
+                db.Entry(add).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                loadData();
+                //this is for make the input empty after pressing add button
+                edit_frist.Text = edit_last.Text = "";
+                //message after the app runs sucessful
+                MessageBox.Show("edited sucessfuly");
+            }
+            catch
+            {
+                MessageBox.Show("Can't edit your Value");
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            id = Convert.ToInt16(dataGridView1.CurrentRow.Cells[0].Value);
+           
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                db = new Database1Entities();
+                add = new TB1();
+                add.Id = id;
+                db.Entry(add).State = System.Data.Entity.EntityState.Deleted;
+                db.SaveChanges();
+
+                loadData();
+                //this is for make the input empty after pressing add button
+                edit_frist.Text = edit_last.Text = "";
+                //message after the app runs sucessful
+                MessageBox.Show("removed sucessfuly");
+            }
+            catch
+            {
+                MessageBox.Show("Can't remove your Value");
+            }
+        }
+    }
+}
